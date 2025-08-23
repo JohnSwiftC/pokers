8d13a6417f42c4c64fe60dbffeba6bf116a7c833
JohnSwiftC <johnswift@live.com>
2025-08-23 16:06:32 -0500

    Add simulation for a calling table, framework for writing sims in the yaml

--- src/main.rs
+++ src/main.rs
@@ -32,9 +32,42 @@ fn main() {
     let doc = &config[0];
 
     if let Yaml::Hash(map) = doc {
-        let mut iter = map.iter();
+        let iter = map.iter();
         for (sim, sim_cfg) in iter {
+            if let Yaml::String(s) = sim {
+                match s.as_str() {
+                    "calling-table" => {
+                        // Praying user doesnt use negatives, i guess it should be a given
+                        let table_size = sim_cfg["table-size"].as_i64().unwrap() as u32;
+                        let initial_bet = sim_cfg["initial-bet"].as_i64().unwrap() as u32;
+                        let initial_pot = sim_cfg["initial-pot"].as_i64().unwrap() as u32;
+                        let show_steps = sim_cfg["show-steps"].as_bool().unwrap_or(true);
 
+                        println!("========================\nCalling Table");
+                        calling_table(table_size, initial_bet, initial_pot, show_steps);
+                        println!("========================");
+                    }
+                    &_ => eprintln!("Test '{}' is not a valid test", s),
+                }
+            }
+        }
+    }
+}
+
+// Shows how pot odds progress for a table as players continue to call
+fn calling_table(table_size: u32, initial_bet: u32, initial_pot: u32, show_steps: bool) {
+    let mut odds = math::pot_odds(initial_bet, initial_pot);
+    if show_steps {
+        for _ in 0..table_size {
+            println!("[#] Call Made - Odds ({})", odds);
+            odds = math::call_series_pot_odds(odds, 1);
+        }
+    } else {
+        if table_size < 2 {
+            println!("Table size not large enough");
+        } else {
+            odds = math::call_series_pot_odds(odds, table_size - 1);
+            println!("[#] Call Made - Odds ({})", odds);
         }
     }
 }
